0fe3941c3a48be2ba746155d6e983cc1d2c49001
Angela Tolwani <angela@tolwani.com>
2024-05-23 13:18:26 -0400

    tests passing

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then(resp => resp.json())
+    .then(q => setQuestions(q))
+  }, [])
+
+  function handleNewQuestion(newQuestion) {
+    setQuestions([...questions, newQuestion])
+  }
+
+  function handleDeletedQuestion(id) {
+    const filteredQuestions = questions.filter(question => question.id !== id)
+    setQuestions(filteredQuestions)
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm onFormSubmit={handleNewQuestion} /> : <QuestionList questions={questions} onQuestionDelete={handleDeletedQuestion} />}
     </main>
   );
 }
